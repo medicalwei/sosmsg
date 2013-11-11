38febd1df1027710c18bf56aaea3b793a4145892
Yao Wei (魏銘廷) <mwei@lxde.org>
2013-11-11 12:09:05 +0800

    use scrollview to preven keyboard cover the text

--- gen/idv/medicalwei/sosmsg/R.java
+++ gen/idv/medicalwei/sosmsg/R.java
@@ -27,17 +27,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_refresh=0x7f09000a;
-        public static final int action_settings=0x7f090009;
+        public static final int action_refresh=0x7f09000b;
+        public static final int action_settings=0x7f09000a;
         public static final int app_status=0x7f090002;
-        public static final int includeGPS=0x7f090007;
+        public static final int includeGPS=0x7f090008;
         public static final int main_vertical=0x7f090000;
-        public static final int message=0x7f090006;
+        public static final int message=0x7f090007;
         public static final int pager=0x7f090001;
-        public static final int send=0x7f090008;
-        public static final int send_status=0x7f090003;
-        public static final int status=0x7f090004;
-        public static final int userName=0x7f090005;
+        public static final int scrollView1=0x7f090003;
+        public static final int send=0x7f090009;
+        public static final int send_status=0x7f090004;
+        public static final int status=0x7f090005;
+        public static final int userName=0x7f090006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
